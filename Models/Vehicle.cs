cbfa65847cd968bf275c07e83d9dc6c2b6d291aa
SamuVD <samuvv2905@gmail.com>
2024-08-02 12:14:59 -0500

    feat: la clase Vehicle quedo full terminada

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -16,27 +16,34 @@ public class Vehicle
     public string? SerialNumber { get; set; }
     public byte PeopleCapacity { get; set; }
     public Driver? Owner { get; set; }
-    public List<Vehicle> vehicles = new List<Vehicle>()
-    {
-        new Vehicle
-        (
-            
-        )
-    };
+    public List<Vehicle> vehicles = new List<Vehicle>() { };
 
 
 
     // Constructor propio de la clase
-    public Vehicle()
+    public Vehicle(int _id, string _licensePlate, string _type, string _engineNumber, string _serialNumber, byte _peopleCapacity, Driver _owner)
     {
-
+        Id = _id;
+        LicensePlate = _licensePlate;
+        Type = _type;
+        EngineNumber = _engineNumber;
+        SerialNumber = _serialNumber;
+        PeopleCapacity = _peopleCapacity;
+        Owner = _owner;
     }
 
 
 
     // Métodos requeridos para el programa propios de esta clase
-    public void DeleteVehicle (int id)
+    public void DeleteVehicle()
     {
-        
+        Console.Write("\nIngresa el ID del vehículo que quieres eliminar => ");
+        int vehicleFoundDelete = Convert.ToInt32(Console.ReadLine());
+
+        var vehicleDelete = vehicles.Find(item => item.Id == vehicleFoundDelete);
+
+        vehicles.Remove(vehicleDelete);
+
+        Console.WriteLine("\n¡EMPLOYEE DELETED CORRECTLY!\n");
     }
 }
